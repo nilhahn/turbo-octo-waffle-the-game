2c4ca25e6adb84d8fe4ce635e68e066a1a1cdd8d
nilhahn <mheisler@outlook.de>
2020-05-22 21:59:52 +0200

    Add untested Vector2D class

---
+++ src/System/Vector2D.cpp
@@ -0,0 +1,45 @@
+//
+// Created by matze on 22.05.20.
+//
+
+#include "Vector2D.h"
+
+#include <cmath>
+
+Vector2D::Vector2D(): x(0), y(0) {}
+
+Vector2D::Vector2D(float x, float y): x(x), y(y) {}
+
+float Vector2D::length() {
+    return std::sqrt(x*x + y*y);
+}
+
+float Vector2D::operator*(Vector2D &vector) {
+    return 0;
+}
+
+Vector2D Vector2D::operator*(float scalar) {
+    return Vector2D(this->x * scalar, this->y * scalar);
+}
+
+Vector2D Vector2D::operator+(Vector2D &vector) {
+    return Vector2D(this->x + vector.x, this->y + vector.y);
+}
+
+Vector2D &Vector2D::operator=(Vector2D vector) {
+    this->x = vector.x;
+    this->y = vector.y;
+    return *this;
+}
+
+Vector2D &Vector2D::operator+=(Vector2D &vector) {
+    this->x += vector.x;
+    this->y += vector.y;
+    return *this;
+}
+
+Vector2D &Vector2D::operator-=(Vector2D &vector) {
+    this->x -= vector.x;
+    this->y -= vector.y;
+    return *this;
+}
---
+++ src/System/Vector2D.h
@@ -0,0 +1,29 @@
+//
+// Created by matze on 22.05.20.
+//
+
+#ifndef TURBO_OCTO_WAFFLE_THE_GAME_VECTOR2D_H
+#define TURBO_OCTO_WAFFLE_THE_GAME_VECTOR2D_H
+
+#include <cmath>
+
+class Vector2D {
+public:
+    Vector2D();
+    Vector2D(float x, float y);
+
+    float length();
+
+    float operator* (Vector2D& vector);
+    Vector2D operator* (float scalar);
+    Vector2D operator+ (Vector2D& vector);
+    Vector2D& operator= (Vector2D vector);
+    Vector2D& operator+= (Vector2D& vector);
+    Vector2D& operator-= (Vector2D& vector);
+private:
+    float x;
+    float y;
+};
+
+
+#endif //TURBO_OCTO_WAFFLE_THE_GAME_VECTOR2D_H
