1323f85dd8c03ff5b5a5600148781e67b575c2bf
nilhahn <matzeheisler@googlemail.com>
2021-05-07 21:38:29 +0200

    Add class to create a camera
    (wip)

---
+++ src/window/Camera.cpp
@@ -0,0 +1,37 @@
+//
+// Created by mheisler on 07.05.21.
+//
+
+#include "Camera.h"
+
+Camera::Camera() {
+}
+
+void Camera::init(int width, int height, Vector2D inital) {
+    this->width = width;
+    this->height = height;
+    this->coord = inital;
+
+    this->center.setX(this->coord.getX() + (this->width/2.));
+    this->center.setY(this->coord.getY() + (this->height/2.));
+}
+
+void Camera::move(Vector2D delta) {
+    this->coord += delta;
+}
+
+int Camera::getWidth() {
+    return this->width;
+}
+
+int Camera::getHeight() {
+    return this->height;
+}
+
+Vector2D Camera::getCenter() {
+    return this->center;
+}
+
+Vector2D Camera::getCoord() {
+    return this->coord;
+}
---
+++ src/window/Camera.h
@@ -0,0 +1,33 @@
+//
+// Created by mheisler on 07.05.21.
+//
+
+#ifndef TURBO_OCTO_WAFFLE_THE_GAME_CAMERA_H
+#define TURBO_OCTO_WAFFLE_THE_GAME_CAMERA_H
+
+
+#include "../System/Vector2D.h"
+
+class Camera {
+public:
+    Camera();
+
+    void init(int width, int height, Vector2D inital);
+    void move(Vector2D delta);
+
+    int getWidth();
+    int getHeight();
+
+    Vector2D getCenter();
+    Vector2D getCoord();
+
+private:
+    Vector2D coord;
+    Vector2D center;
+
+    int width;
+    int height;
+};
+
+
+#endif //TURBO_OCTO_WAFFLE_THE_GAME_CAMERA_H
