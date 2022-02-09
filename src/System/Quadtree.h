c49fbf3ce7c1058c35c3cffa2d4ffd3a02e2e208
nilhahn <mheisler@outlook.de>
2022-02-09 21:27:37 +0100

    add the Quadtree, in order to manage the underlying BackgroundLayer

---
+++ src/System/Quadtree.h
@@ -0,0 +1,206 @@
+#ifndef QUADTREE_QUADTREE_H
+#define QUADTREE_QUADTREE_H
+
+#include <iostream>
+
+#include "Square2D.h"
+
+template<typename V>
+class Quadtree {
+public:
+    typedef enum {NW = 0, NE, SE, SW} Quadrant;
+
+    Quadtree(Square2D& coord, V& v, Quadtree* root);
+    Quadtree(Square2D& coord, V& v);
+    ~Quadtree();
+
+    void insert(Square2D& coord, V& v);
+    void remove(Square2D& coord);
+    bool contains(Square2D& coord);
+    V* find(Square2D const& coord);
+
+    bool isRoot();
+    bool hasChildren();
+    int size();
+protected:
+    static constexpr int NODES = 4;
+
+    Square2D own;
+
+    Quadtree* root;
+    Quadtree* children[NODES];
+
+    int fill{};
+    V value;
+
+    Quadrant getQuadrant(Vector2D& key);
+
+    void initChildren();
+};
+
+template<typename V>
+Quadtree<V>::Quadtree(Square2D &coord, V &value, Quadtree *root) {
+    this->root = root;
+
+    this->own = coord;
+
+    this->value = value;
+    this->initChildren();
+}
+
+template<typename V>
+Quadtree<V>::Quadtree(Square2D &coord, V& value) {
+    this->root = nullptr;
+
+    this->own = coord;
+
+    this->value = value;
+    this->initChildren();
+}
+
+template<typename V>
+Quadtree<V>::~Quadtree() {
+    for (int index = 0; index < NODES; index++) {
+        if (nullptr != children[index]) {
+            delete children[index];
+        }
+        children[index] = nullptr;
+    }
+}
+
+template<typename V>
+void Quadtree<V>::insert(Square2D &coord, V &v) {
+    Quadrant quadrant = this->getQuadrant(coord.getCornerSquare());
+
+    if (coord.isIn(this->own.getCornerSquare())) {
+        return;
+    }
+
+    if (children[quadrant] == nullptr) {
+        children[quadrant] = new Quadtree(coord, v, this);
+        this->fill++;
+    } else {
+        children[quadrant]->insert(coord, v);
+    }
+}
+
+template<typename V>
+bool Quadtree<V>::contains(Square2D &coord) {
+    if (this->own.isIn(coord.getCornerSquare())) {
+        return true;
+    }
+
+    for (auto &child : this->children) {
+        if (child != nullptr && child->contains(coord)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+template<typename V>
+V* Quadtree<V>::find(Square2D const& coord) {
+    if (this->own.isIn(coord.getCornerSquare())) {
+        return &this->value;
+    }
+
+    for (auto &child : this->children) {
+        if (child != nullptr) {
+            auto element = child->find(coord);
+            if(element != nullptr) {
+                return element;
+            }
+        }
+    }
+
+    return nullptr;
+}
+
+template<typename V>
+void Quadtree<V>::remove(Square2D &coord) {
+    Quadtree *temp[NODES] = {nullptr, nullptr, nullptr, nullptr};
+
+    for (int index = 0; index < NODES; index++) {
+        if (this->children[index] != nullptr) {
+            if (this->children[index]->own == coord) {
+                if (this->children[index]->hasChildren()) {
+                    for (int inner = 0; inner < NODES; inner++) {
+                        temp[inner] = this->children[index]->children[inner];
+                        this->children[index]->children[inner] = nullptr;
+                    }
+                }
+
+                delete this->children[index];
+                this->children[index] = nullptr;
+
+                for (auto &child: temp) {
+                    if (child != nullptr) {
+                        if (this->isRoot() == false) {
+                            this->root->insert(child->own, child->value);
+                        } else {
+                            this->insert(child->own, child->value);
+                        }
+                        delete child;
+                    }
+                }
+
+                this->fill--;
+                return;
+            }
+            this->children[index]->remove(coord);
+        }
+    }
+}
+
+template<typename V>
+bool Quadtree<V>::isRoot() {
+    return this->root == nullptr;
+}
+
+template<typename V>
+bool Quadtree<V>::hasChildren() {
+    return this->fill != 0;
+}
+
+template<typename V>
+int Quadtree<V>::size() {
+    int cnt = 0;
+    for (auto &child: children) {
+        if (child != nullptr) {
+            cnt += child->size();
+        }
+    }
+    return ++cnt;
+}
+
+template<typename V>
+void Quadtree<V>::initChildren() {
+    this->fill = 0;
+    for (auto &child : children) {
+        child = nullptr;
+    }
+}
+
+template<typename V>
+typename Quadtree<V>::Quadrant Quadtree<V>::getQuadrant(Vector2D &key) {
+    Quadrant quadrant;
+
+    if (key.getY() >= this->own.getCornerY()) {
+        if (key.getX() < this->own.getCornerX()) {
+            quadrant = NW;
+        } else {
+            quadrant = NE;
+        }
+    } else {
+        if (key.getX() < this->own.getCornerX()) {
+            quadrant = SW;
+        } else {
+            quadrant = SE;
+        }
+    }
+
+    return quadrant;
+}
+
+#endif //QUADTREE_QUADTREE_H
--- src/System/Square2D.cpp
+++ src/System/Square2D.cpp
@@ -3,29 +3,40 @@
 
 int Square2D::count = 0;
 
-Square2D::Square2D(): corner(0,0), width(0), height(0) {}
+Square2D::Square2D() : corner(0, 0), width(0), height(0) {}
 
-Square2D::Square2D(Vector2D& corner_, float width_, float height_): corner(corner_.getX(),corner_.getY()), width(width_), height(height_){
+Square2D::Square2D(float x, float y, float w, float h) : corner(x, y), width(w), height(h) {}
+
+Square2D::Square2D(Vector2D &corner_, float width_, float height_) : corner(corner_.getX(), corner_.getY()),
+                                                                     width(width_), height(height_) {
 }
 
-float Square2D::getCornerX(){
+float Square2D::getCornerX() {
     return this->corner.getX();
 }
 
-float Square2D::getCornerY(){
+float Square2D::getCornerY() {
     return this->corner.getY();
 }
 
-void Square2D::setCorner(Vector2D& corner){
+float Square2D::getCornerX() const {
+    return this->corner.getX();
+}
+
+float Square2D::getCornerY() const {
+    return this->corner.getY();
+}
+
+void Square2D::setCorner(Vector2D &corner) {
     this->setCornerX(corner.getX());
     this->setCornerY(corner.getY());
 }
 
-void Square2D::setCornerX(float cornerX){
+void Square2D::setCornerX(float cornerX) {
     this->corner.setX(cornerX);
 }
 
-void Square2D::setCornerY(float cornerY){
+void Square2D::setCornerY(float cornerY) {
     this->corner.setY(cornerY);
 }
 
@@ -41,23 +52,23 @@ float Square2D::area() {
     return this->height * this->width;
 }
 
-float Square2D::getWidth(){
+float Square2D::getWidth() {
     return this->width;
 }
 
-float Square2D::getHeight(){
+float Square2D::getHeight() {
     return this->height;
 }
 
-void Square2D::setWidth(float width){
+void Square2D::setWidth(float width) {
     this->width = width;
 }
 
-void Square2D::setHeight(float height){
+void Square2D::setHeight(float height) {
     this->height = height;
 }
 
-Square2D& Square2D::operator=(Square2D const& origin) {
+Square2D &Square2D::operator=(Square2D const &origin) {
     this->corner.setX(origin.corner.getX());
     this->corner.setY(origin.corner.getY());
     this->width = origin.width;
@@ -65,22 +76,27 @@ Square2D& Square2D::operator=(Square2D const& origin) {
     return *this;
 }
 
-bool Square2D::isIn(const Vector2D& coord) const {
+bool Square2D::isIn(const Vector2D &coord) const {
     return coord.getX() >= corner.getX() && coord.getY() >= corner.getY() &&
-    coord.getX() < getLowerCornerX() && coord.getY() < getLowerCornerY();
+           coord.getX() < getLowerCornerX() && coord.getY() < getLowerCornerY();
 }
 
-bool Square2D::operator<(const Square2D& square) const{
+bool Square2D::operator<(const Square2D &square) const {
     Square2D::count++;
     return !square.isIn(this->corner);
 }
 
-bool Square2D::operator>(const Square2D& square) const{
+bool Square2D::operator>(const Square2D &square) const {
     Square2D::count++;
     return square.isIn(this->corner);
 }
 
-bool Square2D::operator()(const Square2D& squareA, const Square2D& squareB) {
+bool Square2D::operator()(const Square2D &squareA, const Square2D &squareB) {
     Square2D::count++;
     return squareA < squareB;
 }
+
+Vector2D &Square2D::getCornerSquare() const {
+    return const_cast<Vector2D &>(this->corner);
+}
+
