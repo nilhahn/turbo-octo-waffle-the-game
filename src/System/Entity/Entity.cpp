389f7d12914fd0f3fe95349137182ca9c6759fb3
nilhahn <mheisler@outlook.de>
2022-06-29 08:24:36 +0200

    fix a syntax error

--- src/System/Entity/Entity.cpp
+++ src/System/Entity/Entity.cpp
@@ -11,10 +11,7 @@ Entity::Entity() {
 void Entity::addProperty(BaseProperty *property) {
     if (property != nullptr) {
         std::unique_ptr<BaseProperty> prop(property);
-        this->properties.insert(
-                std::pair<BaseProperty::ID, std::unique_ptr<BaseProperty> >(
-                        static_cast<const BaseProperty::Property_ID>(property->getId()),
-                        prop));
+        this->properties.insert(std::make_pair(property->getId(), std::move(prop)));
     }
 }
 
